5b7d3f4ebce486e7e09304b50303b1c3bde6a9e0
khizaramin403 <khizaramin403@gmail.com>
2026-09-10 11:41:43 -0700

    social links add

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -79,7 +79,7 @@ export default function Footer() {
             {/* Social Links */}
             <div className="flex items-center gap-2.5 pt-2">
               <a
-                href="https://google.com"
+                href="https://www.google.com/search?q=dos+ros+detailing+morris&sca_esv=35cfe427b01f9ce2&sxsrf=APpeQnubABDyQlUKzeujqubn9MHryYxxZA%3A1789064331965&ei=i_SiauWyOr6Xi-gP5P6GqAo&biw=1366&bih=599&ved=2ahUKEwilpPL_z-SWAxW-ywIHHWS_AaUQ4dUDegQIBhAM&uact=5&oq=dos+ros+detailing+morris&gs_lp=Egxnd3Mtd2l6LXNlcnAiGGRvcyByb3MgZGV0YWlsaW5nIG1vcnJpczIIEAAYgAQYogQyBRAAGO8FMggQABiABBiiBDIIEAAYiQUYogQyCBAAGIAEGKIESLwPULMDWIcNcAF4AJABAJgBpwOgAfwOqgEFMy0yLjO4AQPIAQD4AQGYAgagAskPwgIKECMYsAIYsAMYJ8ICDRAAGIAEGIoFGEMYsAPCAgoQABiABBgNGLADwgILEAAYiQUYogQYsAPCAggQABjvBRiwA8ICBxAjGLACGCfCAgcQABiABBgNwgIGEAAYHhgNwgIFECEYnwWYAwDiAwUSATEgQIgGAZAGCJIHBzEuMy0yLjOgB6YcsgcFMy0yLjO4B7sPwgcFMi00LjLIByyACAE&sclient=gws-wiz-serp"
                 target="_blank"
                 rel="noreferrer"
                 aria-label="Google Review and Profile"
@@ -88,7 +88,7 @@ export default function Footer() {
                 <GoogleOutlined className="text-sm" />
               </a>
               <a
-                href="https://facebook.com"
+                href="https://www.facebook.com/DosBrosDetailingMN#"
                 target="_blank"
                 rel="noreferrer"
                 aria-label="Follow Dos Bros Detailing on Facebook"
@@ -97,7 +97,7 @@ export default function Footer() {
                 <FacebookOutlined className="text-sm" />
               </a>
               <a
-                href="https://instagram.com"
+                href="https://www.instagram.com/dosbrosmn"
                 target="_blank"
                 rel="noreferrer"
                 aria-label="Follow Dos Bros Detailing on Instagram"
@@ -106,7 +106,7 @@ export default function Footer() {
                 <InstagramOutlined className="text-sm" />
               </a>
               <a
-                href="https://tiktok.com"
+                href="https://www.tiktok.com/@dosbrosdetailing"
                 target="_blank"
                 rel="noreferrer"
                 aria-label="Follow Dos Bros Detailing on TikTok"
